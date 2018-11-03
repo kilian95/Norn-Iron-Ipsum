6da94cff3ca8d886223b15ba06ab11c0e003c093
Kilian Drayne <kiliandrayne@hotmail.com>
2018-11-03 15:06:30 +0000

    Removed space between full stop and last word in the sentence. Moved the grammar logic into its own seperate functions.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(function() {
   
-	var words = ['bake', 'banjaxed', 'banter', 'beezer', 'big lad', 'bout ye', 'catch yourself on,', 'cracker', 'craic', 'dander', 'dead on,', 'eejit', 'faffin', 'fegs', 'grand', 'kex', 'melter', 'munter', 'mucker', 'offie', 'oul-doll', 'peelers', 'ratten', 'spuds', 'steamin', 'wee', 'what about ye,', 'wind yer neck in,', 'ats me nai,', 'buck eejit', 'gurn', 'yoke', 'foundered', 'quare stretch in the evenings,', 'whats the craic,', 'yer da sells avon,', 'yer mas your da,', 'swall', 'you know yourself,', 'sucking diesel,', 'boyo', 'chancer', 'culchie', 'gaff', 'keep er lit,'];
+	var words = ['bake', 'banjaxed', 'banter', 'beezer', 'big lad', 'bout ye', 'catch yourself on,', 'cracker', 'craic', 'dander', 'dead on', 'eejit', 'faffin', 'fegs', 'grand', 'kex', 'melter', 'munter', 'mucker', 'offie', 'oul-doll', 'peelers', 'ratten', 'spuds', 'steamin', 'wee', 'what about ye,', 'wind yer neck in,', 'ats me nai', 'buck eejit', 'gurn', 'yoke', 'foundered', 'quare stretch in the evenings hi,', 'whats the craic', 'yer da sells avon', 'yer mas your da', 'swall', 'you know yourself,', 'sucking diesel', 'boyo', 'chancer', 'culchie', 'gaff', 'keep er lit,'];
 
 	// Return random number
 	function getSentenceLength() {
@@ -16,31 +16,29 @@ $(function() {
 	function generateSentences() {
 		var index = 0;
 		randomizeArray();
+
 		
-		// Add grammar
+		// Add grammar to each sentence
 		for (i = 0; i < words.length; i += sentenceLength) {
 
-			// Add full stops
     	var sentenceLength = getSentenceLength();
     	var index = index + sentenceLength;
-    	words.splice(index, 0, ". ");
-    	// console.log(sentenceLength);
-    	
-    	//TODO - Instead of adding new element for full stop just add it to the word. 
 
-		
-    	// Add capital letters to first word
-    	var firstWord = words[0].toString(); 
-    	firstWord = firstWord.charAt(0).toUpperCase() + firstWord.slice(1);
-    	words[0] = firstWord;
+    	// Add grammar
+    	addFullStops(words, index);
+    	addCapitalLetters(words, index);
+    	    	
+			// TODO
+			// Remove full stops that appear after commas.
 
-			// Add capital letters to to rest of sentences
-			if (index + 1 < words.length) {
-				firstWord = words[index + 1].toString();
-				firstWord = firstWord.charAt(0).toUpperCase() + firstWord.slice(1);
-				words[index + 1] = firstWord
-			}
-		}
+			// TODO
+			// Add paragraphs
+
+		}  
+
+		// Add full stop to last word
+		var lastWord = words[words.length-1].toString().concat(".");
+   	words[words.length-1] = lastWord;
 
 		// Join contents of array into string
 		var sentence = words.join(' ');
@@ -48,16 +46,30 @@ $(function() {
 		console.log(sentence);
 	}
 
+
+	function addFullStops(words, index) {
+		// Add full stops
+  	// Get last word of sentence and convert to string. Make sure word exists
+  	if (words[index]) {
+  		var endSentence = words[index].toString().concat(".");
+  		words[index] = endSentence
+  	}
+	}
+
+	function addCapitalLetters(words, index) {
+		// Add capital letter to first word
+		var firstWord = words[0].toString(); 
+    	firstWord = firstWord.charAt(0).toUpperCase() + firstWord.slice(1);
+    	words[0] = firstWord;
+
+    // Add capital letter to start of each sentence.
+		if (index + 1 < words.length) {
+			firstWord = words[index + 1].toString();
+			firstWord = firstWord.charAt(0).toUpperCase() + firstWord.slice(1);
+			words[index + 1] = firstWord
+		}
+	}
+
 	generateSentences();
-
-
-
-
-
-
-
-
-
-
 
 });
